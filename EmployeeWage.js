a4662b45091c65b1cae9a966db984b03b4a981a4
Pradnyads <sorepradnya@gmail.com>
2020-11-30 23:58:13 +0530

    Refactor : Total Employee Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,11 +1,14 @@
 console.log('Welcome to Employee Wage Computation')
-//uc2
+
 const IS_PART_TIME = 2;
 const IS_FULL_TIME = 1;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 const NUM_OF_WORKING_DAYS = 20;
+const MAX_HRS_IN_MONTH = 160;
+  let totalEmpHrs = 0;
+  let totalWorkingDays = 0;
 
 function getWorkingHours(empCheck){ 
     switch (empCheck) {
@@ -25,10 +28,12 @@ function getWorkingHours(empCheck){
       let empHrs = 0;
       empCheck = Math.floor(Math.random() * 10) % 3;
 
-      for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+      while(totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS) {
+        totalWorkingDays++;
         let empCheck = Math.floor(Math.random() * 10) % 3;
-           empHrs += getWorkingHours(empCheck);
-        }
-         let empWage = empHrs * WAGE_PER_HOUR;
-         console.log("Total Hrs: " +empHrs+ " emp Wage: " + empWage);
-   
+        totalEmpHrs +=  getWorkingHours(empCheck);
+     }
+        let empWage = totalEmpHrs * WAGE_PER_HOUR;
+        console.log("Total Days: " + totalWorkingDays + "Total Hrs: " +totalEmpHrs+ " Emp Wage: " + empWage);
+ 
+    
