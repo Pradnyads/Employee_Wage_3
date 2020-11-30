d2fabe7da25a5abb7a70a177d6c1d5d482d521d0
Pradnyads <sorepradnya@gmail.com>
2020-11-30 23:45:51 +0530

    Refactor : Monthly Employee Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -5,6 +5,7 @@ const IS_FULL_TIME = 1;
 const PART_TIME_HOURS = 4;
 const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
+const NUM_OF_WORKING_DAYS = 20;
 
 function getWorkingHours(empCheck){ 
     switch (empCheck) {
@@ -24,6 +25,10 @@ function getWorkingHours(empCheck){
       let empHrs = 0;
       empCheck = Math.floor(Math.random() * 10) % 3;
 
-      empHrs = getWorkingHours(empCheck);
-      let empWage = empHrs * WAGE_PER_HOUR;
-      console.log("emp Wage: " + empWage);
+      for (let day = 0; day < NUM_OF_WORKING_DAYS; day++) {
+        let empCheck = Math.floor(Math.random() * 10) % 3;
+           empHrs += getWorkingHours(empCheck);
+        }
+         let empWage = empHrs * WAGE_PER_HOUR;
+         console.log("Total Hrs: " +empHrs+ " emp Wage: " + empWage);
+   
